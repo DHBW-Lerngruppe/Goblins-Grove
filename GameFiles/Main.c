c674990fdd97602d0f934228d117d2780232226d
fpuhlig <felix@familie-uhlig.net>
2024-02-02 14:46:52 +0100

    Added first Controls

--- GameFiles/Main.c
+++ GameFiles/Main.c
@@ -5,7 +5,25 @@
 int main()
 {
 
-    //UI(7);
-    controls();
+    // UI(10);
+    int decision = controls();
+    switch (decision)
+    {
+    case 1:
+        printf("Du gehst ins Dorf\n");
+        break;
+    case 2:
+        printf("Du gehst in den Wald\n");
+        break;
+    case 3:
+        printf("Du gehst in die Höhle\n");
+        break;
+    case 20:
+        printf("Du öffnest dein Inventar\n");
+        break;
+    default:
+        printf("Keine gültige Auswahl!\n");
+        break;
+    }
     return 0;
 }
--- GameFiles/controls.c
+++ GameFiles/controls.c
@@ -1,22 +1,55 @@
 #include <stdio.h>
-#include <controls.h>
+#include "controls.h"
+
+int reisen();
+
 
 int controls()
 {
-    int decision;
+    int decision = 0;
+    int finalDecision = 0;
     printf("Wähle aus folgenden Optionen aus:\n");
     printf("1 - Reisen\n");
-    scanf("%d", decision);
+    printf("2 - Inventar öffnen\n");
+    scanf("%d", &decision);
 
     switch (decision)
     {
     case 1:
-        printf("Du begibst dich auf eine Reise.\n");
+        finalDecision = reisen();
         break;
-    
+    case 2:
+        finalDecision = 20;
+        break;
+
     default:
-        printf("Keine Gültige Auswahl!\n");
         break;
     }
+
+    return finalDecision;
 }
 
+int reisen()
+{
+    int decision = 0;
+
+    printf("\nWohin möchtest du reisen?\n");
+    printf("1 - Dorf\n");
+    printf("2 - Wald\n");
+    printf("3 - Höhle\n");
+
+    scanf("%d", &decision);
+
+    switch (decision)
+    {
+    case 1:
+        break;
+    case 2: 
+        break;
+    case 3: 
+        break;
+    default:
+        break;
+    }
+    return decision;
+}
